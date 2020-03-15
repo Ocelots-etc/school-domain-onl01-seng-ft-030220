b92877e00d6043189990ea87a34ffc28da8a6cc0
Cat Gomez <catherinedgomez@gmail.com>
2020-03-15 05:49:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,16 @@
 class School
-attr_accessor :name, :roster, :grade
-
-roster = {}
-
-def initialize(roster)
-  @roster = roster
-  roster["name"] << roster
-end
-
-def add_student(name, grade)
-
-end
+ attr_accessor :name
+# , :roster, :grade
+#
+# roster = {}
+#
+# def initialize(roster)
+#   @roster = roster
+#   roster["name"] << roster
+# end
+#
+# def add_student(name, grade)
+#
+# end
 
 end
